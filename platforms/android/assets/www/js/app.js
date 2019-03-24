263b46f4bfe096565e3139feb09564c81f50f615
gopinath <gopinathkaliappan@gmail.com>
2019-03-24 22:41:29 +0530

    sdasd

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -23,92 +23,92 @@
 
 
 var app = angular.module('slidebox', ['ionic', 'tabSlideBox','ngCordova', 'xml', 'rss-parser'])
-		.run(['$q', '$http', '$rootScope', '$location', '$window', '$timeout', 
-					function($q, $http, $rootScope, $location, $window, $timeout){
-	    
-	        $rootScope.$on("$locationChangeStart", function(event, next, current){
-	            $rootScope.error = null;
-	            console.log("Route change!!!", $location.path());
-	            var path = $location.path();
-	            
-	            
-	            console.log("App Loaded!!!");
-	        });
-	    }
-	    ]);
+    .run(['$q', '$http', '$rootScope', '$location', '$window', '$timeout', 
+          function($q, $http, $rootScope, $location, $window, $timeout){
+      
+          $rootScope.$on("$locationChangeStart", function(event, next, current){
+              $rootScope.error = null;
+              console.log("Route change!!!", $location.path());
+              var path = $location.path();
+              
+              
+          });
+      }
+      ]);
 
-		app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
-			$stateProvider.state('index', {
-				url : '/',
-				templateUrl : 'index.html',
-				controller : 'IndexCtrl'
-			});
-		      
-			$urlRouterProvider.otherwise("/");
+    app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+      $stateProvider.state('index', {
+        url : '/',
+        templateUrl : 'index.html',
+        controller : 'IndexCtrl'
+      });
+          
+      $urlRouterProvider.otherwise("/");
             $httpProvider.interceptors.push('xmlHttpInterceptor')
-		});
-		
+    });
+    
         app.controller("IndexCtrl", ['$rootScope', "$scope", "$stateParams", "$q", "$location", "$window", '$timeout', 
-			function($rootScope, $scope, $stateParams, $q, $location, $window, $timeout){
-			$scope.onSlideMove = function(data){
-				//alert("You have selected " + data.index + " tab");
-			};	
+      function($rootScope, $scope, $stateParams, $q, $location, $window, $timeout){
+      $scope.onSlideMove = function(data){
+        //alert("You have selected " + data.index + " tab");
+      };  
         }
         ])
 
         var app = angular.module('slidebox', ['ionic', 'tabSlideBox','ngCordova'])
-		.run(['$q', '$http', '$rootScope', '$location', '$window', '$timeout', 
-					function($q, $http, $rootScope, $location, $window, $timeout){
-	    
-	        $rootScope.$on("$locationChangeStart", function(event, next, current){
-	            $rootScope.error = null;
-	            console.log("Route change!!!", $location.path());
-	            var path = $location.path();
-	            
-	            
-	            console.log("App Loaded!!!");
-	        });
-	    }
-	    ]);
-		
-		app.config(function($stateProvider, $urlRouterProvider) {
-			$stateProvider.state('index', {
-				url : '/',
-				templateUrl : 'index.html',
-				controller : 'IndexCtrl'
-			});
-		
-			$urlRouterProvider.otherwise("/");
-		});
-		
+    .run(['$q', '$http', '$rootScope', '$location', '$window', '$timeout', 
+          function($q, $http, $rootScope, $location, $window, $timeout){
+      
+          $rootScope.$on("$locationChangeStart", function(event, next, current){
+              $rootScope.error = null;
+              console.log("Route change!!!", $location.path());
+              var path = $location.path();
+              
+              
+              console.log("App Loaded!!!");
+          });
+      }
+      ]);
+    
+    app.config(function($stateProvider, $urlRouterProvider) {
+      $stateProvider.state('index', {
+        url : '/',
+        templateUrl : 'index.html',
+        controller : 'IndexCtrl'
+      });
+    
+      $urlRouterProvider.otherwise("/");
+    });
+    
         app.controller("IndexCtrl", ['$rootScope', "$scope", "$stateParams", "$q", "$location", "$window", '$timeout', 
-			function($rootScope, $scope, $stateParams, $q, $location, $window, $timeout){
+      function($rootScope, $scope, $stateParams, $q, $location, $window, $timeout){
 
 
  
  
 
-			$scope.onSlideMove = function(data){
+      $scope.onSlideMove = function(data){
                     
-                 			
-			};	
+                      
+      };  
         }
         ])
 
-app.controller("FeedController", function($http, $scope,$timeout,$ionicPlatform,$cordovaSocialSharing) {
+app.controller("FeedController", function($http, $scope,$timeout,$ionicPlatform,$cordovaSocialSharing,$ionicSlideBoxDelegate) {
   
 
-        $scope.pageCount = 1;
-        $scope.loadMore = () => {
-            $scope.pageCount = Number($scope.pageCount) + 1;
-        } 
 
  // $scope.checkStatus = (currentIndex) => {
  //  return $scope.count <= currentIndex
  // }
+
   let parser = new RSSParser();
   $scope.entries = [];
   $scope.api = 'https://agaramnews.herokuapp.com?url=';
+  // $scope.api = 'http://localhost:3000?url=';
+  // $scope.tabsApi = 'https://agaramnews.herokuapp.com/tabs';
+  $scope.tabsApi = 'https://agaramnews.herokuapp.com/tabs';
+  // $scope.tabsApi = 'http://localhost:3000/tabs';
   $scope.pagination = '&page=0count=20';
   $scope.apiKey = '&api_key=hpi0ghvdfyy5xbbiwuruga2i6p49cnf0jilgt3dg';
   $scope.count = '&count=100';
@@ -116,121 +116,171 @@ app.controller("FeedController", function($http, $scope,$timeout,$ionicPlatform,
   const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
   $scope.currentIndex = 1;
   $scope.currentItems = [];
-  $scope.tabs = [
-    {
-        name: 'India',
-        url: 'https://www.news18.com/rss/india.xml',
-        icon: '',
-        color: 'purple',
-        text: 'white'
-    },
-    {
-        name: 'Word',
-        url: 'https://www.news18.com/rss/world.xml',
-        icon: '',
-        color: '#C70039',
-        text: 'white'
-    },
-    {
-        name: 'Cricket',
-        url: 'https://www.news18.com/rss/cricketnext.xml',
-        icon: '',
-        color: '#900C3F',
-        text: 'white'
-    },
-     {
-        name: 'BBC Music',
-        url: 'http://www.bbc.co.uk/music/genres/classical/reviews.rss',
-        icon: '',
-        color: '#581845',
-        text: 'white'
-    },
 
-    {
-        name: 'Life Style',
-        url: 'https://www.news18.com/rss/lifestyle.xml',
-        icon: '',
-        color: '#922B21',
-        text: 'white'
-    },
-    {
-        name: 'Fashion',
-        url: 'https://www.huffpost.com/section/business/feed',
-        icon: '',
-        color: '#4A235A',
-        text: 'white'
-    },
-    {
-        name: 'Movies',
-        url: 'https://www.news18.com/rss/movies.xml',
-        icon: '',
-        color: 'grey',
-        text: 'white'
-    },
-        {
-        name: '',
-        url: 'https://www.news18.com/rss/movies.xml',
-        icon: '',
-        color: 'grey',
-        text: 'white'
-    }
-  ];
-          
-            $scope.onSlideMove = (dataId) => {
+  $scope.removeByKey = (array, params)=> {
+    array.some((item, index)=> {
+      return (array[index][params.key] === params.value) ? !!(array.splice(index, 1)) : false;
+    });
+    return array;
+  }
+
+
+   $scope.toggleItems = (id, status, item) => {
+        if(!status) {
+          $scope.tabs.push(item); 
+          window.localStorage.setItem('tabs', JSON.stringify($scope.tabs));
+           // $scope.currentIndex -= 1;  
+        } else {
+           $scope.tabs = $scope.removeByKey($scope.tabs, { key: 'id', value: id })
+           window.localStorage.setItem('tabs', JSON.stringify($scope.tabs));
+           $scope.currentIndex += 0;
+        }
+        // $scope.tabs = $scope.removeByKey($scope.tabs, { key: 'id', value: id })
+   }
+   // $scope.closePopup = () => {
+   //      $scope.myPopup.hide();
+   // } 
+    $scope.showPopup = function() {
+      $scope.data = {}
+        fetch($scope.tabsApi).then((res)=> res.json()).then((response)=> {
+                              $scope.$apply( () => {
+                              $scope.tabListing = response;
+                              // window.localStorage.setItem('tabs', JSON.stringify($scope.tabs));
+             })   
+                            })
+      // Custom popup
+      $scope.myPopup = $ionicPopup.show({
+         template: `<ion-view> 
+         <ion-content overflow-scroll="true" has-bouncing="true" class="top-space"> 
+                       <i class="icon ion-android-close close-new" ng-click="closePopup()"></i> 
+                       
+                        <div class="list-news">
+<ion-list>                          
+  <div ng-repeat="t in tabListing" class="item item-avatar text-left" ng-click="toggleItems(t.id, isPresent(t.id), t)"> 
+    <img ng-src="{{t.channelImage}}">
+    <h2>{{t.name}}</h2>
+    <h3>{{t.channel}} <i ng-if="isPresent(t.id)" class ="icon ion-checkmark green"></i><i ng-if="!isPresent(t.id)" class ="icon ion-checkmark"></i></h3>
+     
+
+</div>
+</ion-list>
+                        </ion-content>
+                       </ion-view>
+                        `,
+         title: 'Add New Channels',
+         subTitle: 'Add your favourite channels stay updated',
+         scope: $scope,
+      });
+
+      $scope.myPopup.then(function(res) {
+         console.log('Tapped!', res);
+      });    
+      }; 
+      $scope.closePopup =() => {
+        window.location.reload();
+        
+        $scope.myPopup.close();
+      }
+   $scope.loadTabs = () => {
+      if(window.localStorage.getItem('tabs') === null) {
+        
+          fetch($scope.tabsApi).then((res)=> res.json()).then((response)=> {
+            $scope.$apply( () => {
+              $scope.tabs = response;
+              window.localStorage.setItem('tabs', JSON.stringify($scope.tabs));
+            }) 
+          }) 
+      } else {
+        
+             $scope.tabs = JSON.parse(window.localStorage.getItem('tabs'));
+      }  
+    }  
+    
+    $scope.loadTabs();
+
+    $scope.pageCount = 1;
+    $scope.loadMore = () => {
+        $scope.pageCount = Number($scope.pageCount) + 1;
+    } 
+    $scope.isPresent = (id) => {
+      return $scope.tabs.filter((item) => item.id === id).length > 0 
+    }  
+
+            $scope.onSlideMove = (dataNew) => {
+                
+                try {
+                        let dataId = {
+                              index: $scope.tabs[dataNew.index].id 
+                        }     
+                          $scope.$apply( () => {
+
+                        $scope.pageCount = 1;
+                        $scope.currentIndex = dataId.index;
+                        $scope.loadData(dataId);  
+
+                    }) 
+
+                } catch(e) {
+                        fetch($scope.tabsApi).then((res)=> res.json()).then((response)=> {
+                              $scope.$apply( () => {
+                              $scope.tabListing = response;
+                              // window.localStorage.setItem('tabs', JSON.stringify($scope.tabs));
+                          }) 
+                      })  
+                }
+ 
+                // alert($scope.tabs[dataId.index].id);
+
+                $ionicSlideBoxDelegate.update();                 
                   //count = 0;
-                  $scope.pageCount = 1;
-                  $scope.loadData(dataId);  
+                 // if($scope.tabs[dataNew.index].id === undefined) {
+                 
+                 
+                 // } else {
+                  
+                 // }
+
+                 
             };
 
 
             $scope.loadData = (dataId) => {
-                            $scope.currentIndex = dataId.index;
+
               if(window.localStorage.getItem(dataId.index) !== null ) {
-                         // window.localStorage[dataId.index] = JSON.stringify($scope.data[dataId.index]);
-                             $scope.data[dataId.index] = JSON.parse(window.localStorage.getItem(dataId.index));                             
-                          // $scope.data.map((data, i) => {
-                          //    if( i == dataId.index) {
-                          //       $scope.$apply( () => {
-                          //          $scope.data[dataId.index] = JSON.parse(window.localStorage.getItem(dataId.index));        
-                          //       });
-                          //    } else {
-                          //       $scope.data[i] = [];
-                          //    }
-                          // })
-              }
-
-
-  
-              fetch($scope.api + $scope.tabs[dataId.index].url + $scope.pagination).then((res)=> res.json()).then((result) =>{
-                 
-              
-
-
-                 //console.log(feed.title);
+                  $scope.data[$scope.tabs.filter(item => item.id === dataId.index)[0].id] = JSON.parse(window.localStorage.getItem($scope.tabs.filter(item => item.id === dataId.index)[0].id));                             
+              }  
+              fetch($scope.api + $scope.tabs.filter(item => item.id === dataId.index)[0].url).then((res)=> res.json()).then((result) =>{
                  var data = [];
                  let items  = result.rss.channel.item;
-                 items.forEach(function( entry, i) {
-                // console.log(entry.title + ':' + entry.link);
-                     entry.img = getImage(entry.description._cdata = entry.description._cdata);
-                     data.push(entry);
-                        
-                
-                 })
+                 try {
+                   items.forEach(function( entry, i) {
+                  // console.log(entry.title + ':' + entry.link);
+                       entry.img = getImage(entry.description._cdata = entry.description._cdata);
+                       data.push(entry);
+                          
+                  
+                   })
+                 } catch(e) {
+                     // fetch($scope.tabs[dataId.index].url,
+                     //  {headers : { 
+                     //        'Content-Type': 'application/json',
+                     //        'Accept': 'application/xml'
+                     //  }}).then(res=> res.json()).then((response)=>     {
+                     //      console.log(response);  
+                     // })
 
+
+                 }
                  $scope.$apply( () => {
-                    $scope.data[dataId.index]=data; 
+                    $scope.data[$scope.tabs.filter(item => item.id === dataId.index)[0].id]=data; 
 
                  })
                  if($scope.data[dataId.index]) {
 
-                  window.localStorage.setItem(dataId.index, JSON.stringify($scope.data[dataId.index]));
+                      window.localStorage.setItem($scope.tabs.filter(item => item.id === dataId.index)[0].id, JSON.stringify($scope.data[$scope.tabs.filter(item => item.id === dataId.index)[0].id]));
               
                  }
-                 //    setTimeout(()=> {
-                 //   $scope.data[dataId.index]=data;
-                 // }, 1500  )
-                 // window.localStorage['india']=angular.toJson($scope.india);
-               })                   
+              })                   
             }
 try {
 
@@ -245,8 +295,9 @@ try {
 
 $scope.sharenews=function(play,subject,link){
  $cordovaSocialSharing
-    .share(play, 'mesage','https://lh3.googleusercontent.com/CLlK_q4Z2rcCFD6MtnEO8ufvl6SIvbGcfPkPb-gRfRNv9XInwYZgZLLXKZRgpF9fzw=w300-rw', link) // Share via native share sheet
+    .share(play,suject, link) // Share via native share sheet
     .then(function(result) {
+
       // Success!
     }, function(err) {
       // An error occured. Show a message to the user
@@ -260,11 +311,29 @@ $scope.sharenews=function(play,subject,link){
 
 
 
-
+$scope.shareWhatsapp = (message, image, link) => {
+   $cordovaSocialSharing
+    .shareViaWhatsApp(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+}
+$scope.shareFacebook = (message, image, link) => {
+    $cordovaSocialSharing
+    .shareViaFacebook(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+}
 
 $scope.viewnews=function(link)
 {
-    window.open(link, '_blank', 'location=no','hardwareback=no','clearcache=yes');
+
+    window.open(link[Object.keys(link)[0]], '_blank', 'location=no','hardwareback=no','clearcache=yes');
 }
 // $scope.init();
 
@@ -275,12 +344,13 @@ $scope.viewnews=function(link)
 
 
 });
+
 app.directive('imageonload', function() {
     return {
         restrict: 'A',
-        link: function(scope, element, attrs) {
-            element.bind('load', function() {
-                
+        link: (scope, element, attrs) => {
+            element.bind('load', () => {
+                element.src = element.src;
             });
         }
     };
